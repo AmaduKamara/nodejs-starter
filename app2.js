34833923479602fbbac3efbf792dee61e2c12b9f
Amkam <amaduskamara@gmail.com>
2022-05-21 12:12:37 +0000

    Creating and exporting modules

---
+++ app2.js
@@ -0,0 +1,6 @@
+const firstName = require("./utils");
+const add = require("./utils");
+
+const sum = add(2, 6);
+console.log(sum);
+// console.log(firstName);
---
+++ utils.js
@@ -0,0 +1,9 @@
+console.log("Utils.js");
+
+const firstName = "Amadu";
+
+const add = (num1, num2) => {
+  return num1 + num2;
+};
+
+module.exports = add;
