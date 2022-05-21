64e63accfba5942f10973cef3cc641e71a8d1812
Amkam <amaduskamara@gmail.com>
2022-05-21 12:19:40 +0000

    Create a getNotes function and called in as a module

--- app2.js
+++ app2.js
@@ -1,6 +1,9 @@
 const firstName = require("./utils");
 const add = require("./utils");
+const getNotes = require("./notes")
 
 const sum = add(2, 6);
+const notes = getNotes()
 console.log(sum);
+console.log(notes);
 // console.log(firstName);
